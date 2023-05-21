d9f9034fad2115cdb9e5b8308be397ea5d8ed4a6
yasincan691 <yasincan.691@gmail.com>
2023-05-21 22:16:32 +0300

    WebProje

--- Proje/connection.php
+++ Proje/connection.php
@@ -1,8 +1,8 @@
 <?php 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $db_name = "database1";  
+    $servername = "sql101.epizy.com";
+    $username = "epiz_34255831";
+    $password = "DK1dn0zAC5";
+    $db_name = "epiz_34255831_database1";  
     $conn = new mysqli($servername, $username, $password, $db_name, 3306);
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
